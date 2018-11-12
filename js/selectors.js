e837c3d4aa32922c1e3f315fea83a589e199bf96
Nick Smith <smthnck@gmail.com>
2018-11-12 19:56:13 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -15,5 +15,9 @@ function divSelector(){
 }
 
 function firstListItem(){
+<<<<<<< HEAD
   return $('#pic-list li:first-child')
+=======
+  return $('ul #pic-list:first-child')
+>>>>>>> c0041a5bd690bb8e71ef915155f5788f9b842d28
 }
